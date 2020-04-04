b3fbbe860ae81c498b93f570b8de1d8842c46bf8
Marvin Kome <marvinkome@gmail.com>
2020-04-04 12:11:33 +0100

    Setup flutter and add bottom bar of app

---
+++ lib/app.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/screens/home.dart';
+
+class AppRoot extends StatefulWidget {
+  @override
+  _AppRootState createState() => _AppRootState();
+}
+
+class _AppRootState extends State<AppRoot> {
+  int _currentTabIndex = 0;
+  final List<Widget> _children = [
+    Home(),
+    Center(
+      child: Text('This is the search page'),
+    ),
+    Center(
+      child: Text('This is the library page'),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Center(
+            child: Text('Hello World'),
+          )),
+          AppBottom(_currentTabIndex, (int index) => setState(() => _currentTabIndex = index))
+        ],
+      ),
+    );
+  }
+}
+
+class AppBottom extends StatelessWidget {
+  final int currentTabIndex;
+  final void Function(int) onTap;
+
+  AppBottom(this.currentTabIndex, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          // progress bar
+          SizedBox(height: 2.0, child: LinearProgressIndicator(value: 0.4)),
+
+          // now playing
+          Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                border: Border(bottom: BorderSide(width: 1.0, color: Color(0xFFFF000000)))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                // favourite icon
+                IconButton(
+                  onPressed: () => null,
+                  icon: Icon(Icons.favorite),
+                ),
+
+                // song title and artist
+                Text('Lit . Kiasmos'),
+
+                // play/pause Icon
+                IconButton(
+                  onPressed: () => null,
+                  icon: Icon(Icons.play_arrow),
+                ),
+              ],
+            ),
+          ),
+
+          // bottom navigation
+          BottomNavigationBar(
+            onTap: onTap,
+            currentIndex: this.currentTabIndex,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+              BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
+              BottomNavigationBarItem(icon: Icon(Icons.library_music), title: Text('Your Library')),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// _children[_currentTabIndex]
+// BottomNavigationBar(
+//             onTap: onTap,
+//             currentIndex: this.currentTabIndex,
+//             items: [
+//               BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+//               BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
+//               BottomNavigationBarItem(icon: Icon(Icons.library_music), title: Text('Your Library')),
+//             ],
+// )
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+
+class MainApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spotify Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.green[600],
+        accentColor: Colors.white,
+      ),
+
+      //
+      home: AppRoot(),
+    );
+  }
+}
+
+void main() => runApp(MainApp());
